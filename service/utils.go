dd75c7495e90e7a818932afe05a66a1c1bc80da8
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-07-20 23:00:58 +0530

    Refactored url validation and added tests

--- service/utils.go
+++ service/utils.go
@@ -10,10 +10,14 @@ var (
 	playlistRegex = regexp.MustCompile(`(?:https?:\/\/)?(?:www\.)?youtube\.com\/playlist\?list=([a-zA-Z0-9_-]+)`)
 )
 
-func validateURL(url string) (bool, error) {
+func validateURL(url string) error {
 	if !strings.Contains(url, "youtu") || !strings.ContainsAny(url, "\"?&/<%=") {
-		return false, errors.New("not a valid link")
+		return errors.New("not a valid link")
 	}
 
-	return playlistRegex.MatchString(url), nil
+	return nil
+}
+
+func isPlaylistURL(url string) bool {
+	return playlistRegex.MatchString(url)
 }
--- service/utils_test.go
+++ service/utils_test.go
@@ -17,19 +17,35 @@ func Test_validateURL(t *testing.T) {
 		name   string
 		url    string
 		expErr error
-		want   bool
 	}{
-		{name: "playlist url", url: plURL, want: true, expErr: nil},
-		{name: "video url", url: vidURL, want: false, expErr: nil},
-		{name: "invalid url", url: "www.lazy.com", want: false, expErr: errors.New("not a valid link")},
+		{name: "playlist url", url: plURL, expErr: nil},
+		{name: "video url", url: vidURL, expErr: nil},
+		{name: "invalid url", url: "www.lazy.com", expErr: errors.New("not a valid link")},
 	}
 
 	for i, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			isPlayList, err := validateURL(tt.url)
+			err := validateURL(tt.url)
 
 			assert.Equalf(t, tt.expErr, err, "TEST[%d] Failed - %s", i, tt.name)
-			assert.Equalf(t, tt.want, isPlayList, "TEST[%d] Failed - %s", i, tt.name)
+		})
+	}
+}
+
+func Test_isPlaylistURL(t *testing.T) {
+	tests := []struct {
+		name string
+		url  string
+		want bool
+	}{
+		{name: "playlist", url: plURL, want: true},
+		{name: "video", url: vidURL, want: false},
+	}
+
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isPlaylistURL(tt.url)
+			assert.Equalf(t, tt.want, got, "TEST[%d] Failed - %s", i, tt.name)
 		})
 	}
 }
