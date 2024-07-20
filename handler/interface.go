e982f33c44d2d3a07560bdf53fd407df9143f774
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-07-20 23:03:08 +0530

    Cleaned the servicer

--- handler/interface.go
+++ handler/interface.go
@@ -1,14 +1,12 @@
 package handler
 
 import (
-	"context"
 	"ytdl_http/models"
 )
 
 type Servicer interface {
 	GetStatus(videoID string) string
-	GetInfo(url string) ([]models.VideoData, error)
+	GetInfo(url string) ([]models.Video, error)
 	DownloadInfo(videoID string) ([]string, error)
-	DownloadAudio(ctx context.Context, id string) error
-	DownloadVideo(ctx context.Context, id, qual string) error
+	Download(id, qual, audioOnly string) error
 }
