030125f41d3087a997f652e39bee9f1e0443e06f
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-07-22 22:41:27 +0530

    Refactored handler layer

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"ytdl_http/models"
 
@@ -10,15 +9,15 @@ import (
 
 // Handler type contains service type for dependency injection.
 type Handler struct {
-	player   map[string]bool
-	servicer Servicer
+	player  map[string]bool
+	service Servicer
 }
 
 // New to init the handler.
 func New(s Servicer) *Handler {
 	return &Handler{
-		player:   make(map[string]bool, 0),
-		servicer: s,
+		player:  make(map[string]bool, 0),
+		service: s,
 	}
 }
 
@@ -66,7 +65,7 @@ func (h *Handler) Play(c echo.Context) error {
 // Status returns the download status of a video
 func (h *Handler) Status(c echo.Context) error {
 	vID := c.QueryParam("vid")
-	status := h.servicer.GetStatus(vID)
+	status := h.service.GetStatus(vID)
 
 	return c.String(http.StatusOK, status)
 }
@@ -74,24 +73,17 @@ func (h *Handler) Status(c echo.Context) error {
 // GetInfo retrives the information about the url provided.
 func (h *Handler) GetInfo(c echo.Context) error {
 	url := c.FormValue("URL")
-	if url == "" {
-		return c.Render(http.StatusOK, "error", map[string]string{"error": "url is empty"})
-	}
 
-	data, err := h.servicer.GetInfo(url)
+	data, err := h.service.GetInfo(url)
 	if err != nil {
 		return c.Render(http.StatusOK, "error", map[string]string{"error": err.Error()})
 	}
 
-	resp := map[string][]models.VideoData{
+	d := map[string][]models.Video{
 		"Data": data,
 	}
 
-	if len(data) > 1 {
-		return c.Render(http.StatusOK, "playlistInfo", resp)
-	}
-
-	return c.Render(http.StatusOK, "videoInfo", resp)
+	return c.Render(http.StatusOK, "info", d)
 }
 
 // Download start download process based on quality and videoID
@@ -100,32 +92,26 @@ func (h *Handler) Download(c echo.Context) error {
 	id := c.FormValue("id")
 	audioOnly := c.FormValue("audioOnly")
 
-	switch audioOnly {
-	case "":
-		if err := h.servicer.DownloadVideo(c.Request().Context(), id, qual); err != nil {
-			return c.String(http.StatusBadRequest, "Error h.DownloadVideo: "+err.Error())
-		}
-	case "true":
-		if err := h.servicer.DownloadAudio(c.Request().Context(), id); err != nil {
-			return c.String(http.StatusBadRequest, "Error h.DownloadAudio: "+err.Error())
-		}
+	if err := h.service.Download(id, qual, audioOnly); err != nil {
+		c.Logger().Errorf("Download err: %s", err.Error())
+
+		return err
 	}
 
-	css := "text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-	msg := fmt.Sprintf(`<p id='%s' class="text-green-500">Download Started!!</p> <button type='button' hx-get='/status?vid=%s' hx-target="#%s" class='%s'>Get Status</button>`, id, id, id, css)
-
-	return c.String(http.StatusOK, msg)
+	return c.Render(http.StatusOK, "status", map[string]interface{}{
+		"ID": id})
 }
 
 func (h *Handler) DownloadInfo(c echo.Context) error {
-	vidID := c.QueryParam("id")
-	data, err := h.servicer.DownloadInfo(vidID)
+	videoID := c.QueryParam("id")
+
+	qualities, err := h.service.DownloadInfo(videoID)
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
 
 	return c.Render(http.StatusOK, "downloadInfo", map[string]interface{}{
-		"VideoID":    vidID,
-		"VidQuality": data,
+		"ID":         videoID,
+		"VidQuality": qualities,
 	})
 }
