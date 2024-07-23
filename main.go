43c11cf4605b1e883da20a7e9365bbeca685c597
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-07-23 22:23:59 +0530

    Fixed linters and sorted imports

--- main.go
+++ main.go
@@ -4,30 +4,32 @@ import (
 	"net/http"
 	"os"
 	"time"
+
 	"ytdl_http/client"
 	"ytdl_http/handler"
 	"ytdl_http/models"
 	"ytdl_http/service"
 
-	dlr "github.com/kkdai/youtube/v2/downloader"
 	"golang.org/x/time/rate"
 
 	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	dlr "github.com/kkdai/youtube/v2/downloader"
 )
 
 func main() {
 	e := echo.New()
-	// pprof.Register(e)
-
 	t := models.NewTemplate("html")
+
 	e.Renderer = t
 
 	dirPath := "./Downloads/"
-	if _, err := os.Stat(dirPath); os.IsNotExist(err) { // Check if directory exists
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
 		if err := os.Mkdir(dirPath, 0755); err != nil {
 			e.Logger.Errorf("error while creating the directory: %s", err.Error())
+
 			return
 		}
 	}
@@ -60,14 +62,13 @@ func initServices(dir string) *handler.Handler {
 }
 
 func addMiddleWares(app *echo.Echo) {
+	app.Pre(middleware.RemoveTrailingSlash())
 	app.Use(middleware.Logger())
 	app.Use(echoprometheus.NewMiddleware("ytdl_http"))
 	app.Use(middleware.Recover())
-	app.Pre(middleware.RemoveTrailingSlash())
 	app.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(
 		rate.Limit(160),
 	)))
-
 	app.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
 		Skipper:      middleware.DefaultSkipper,
 		ErrorMessage: "Timed-out due to no activity for 3 mins",
